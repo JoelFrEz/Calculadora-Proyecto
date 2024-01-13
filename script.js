c8b3d066bd98cc8565e011940b0e74e5a9deaa08
JoelFrEz <156509180+JoelFrEz@users.noreply.github.com>
2024-01-13 12:08:02 +0100

    add arrow function on all buttons

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const display = document.querySelector("#display-screen");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == "clear") {
+            display.innerText = "";
+        } else if (item.id == "backspace") {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (display.innerText != "" && item.id == "equal") {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == "" && item.id == "equal") {
+            display.innerText = "has un calculo";
+            setTimeout(() => (display.innerText = ""), 2000);
+        } else {
+            display.innerText += item.id;
+        }
+    };
+});
